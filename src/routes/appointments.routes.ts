51d8d30bb5744492c0aaf3e918aab58935b06560
João C. L. Lira <joao7lira@gmail.com>
2020-06-05 20:31:15 -0300

    Commit inicial

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentRouter = Router();
+
+appointmentRouter.get('/', async (request, response) => {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    const appointments = await appointmentsRepository.find();
+
+    return response.json(appointments);
+});
+
+appointmentRouter.post('/', async (request, response) => {
+    try {
+        const { provider, date } = request.body;
+
+        const parsedDate = parseISO(date);
+
+        const appointment = await new CreateAppointmentService().execute({
+            provider,
+            date: parsedDate,
+        });
+
+        return response.json(appointment);
+    } catch (error) {
+        return response.status(400).json({ error: error.message });
+    }
+});
+
+export default appointmentRouter;
---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,38 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+import Appointment from '../models/Appointment';
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+
+interface RequestDTO {
+    provider: string;
+    date: Date;
+}
+
+class CreateAppointmentService {
+    public async execute({ provider, date }: RequestDTO): Promise<Appointment> {
+        const appointmentsRepository = getCustomRepository(
+            AppointmentRepository,
+        );
+
+        const appointmentDate = startOfHour(date);
+
+        const checkDate = await appointmentsRepository.findByDate(
+            appointmentDate,
+        );
+
+        if (checkDate) {
+            throw Error('Este horário não está disponível.');
+        }
+
+        const appointment = appointmentsRepository.create({
+            provider,
+            date: appointmentDate,
+        });
+
+        await appointmentsRepository.save(appointment);
+
+        return appointment;
+    }
+}
+
+export default CreateAppointmentService;
